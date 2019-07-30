4c12b34143b3384ece612adecdf6603cf5c70633
Stuy-Kevin-Lin <klin14@stuy.edu>
2019-07-29 20:16:44 -0400

    Database functions work

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+import json
 from os import urandom
 
-from flask import request, render_template, redirect
+from flask import Flask, request, render_template, redirect
+from util import Database
+
+config = json.load(open("config/mongo.json"))
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = config['databaseName']
+app.config["MONGO_URI"] = config['mongoURI']
 app.config["SECRET_KEY"] = urandom(32)
+
+dbtools = Database.DBTools(app)
 
 @app.route("/")
 def index():
@@ -16,6 +23,6 @@ def admin():
     return render_template("admin_home.html")
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    # app.debug = True
+    # app.run()
 
--- util/Database.py
+++ util/Database.py
@@ -3,38 +3,35 @@ from flask_pymongo import PyMongo
 
 CHARSET = set("1234567890QWERTYUIOPASDFGHJKLZXCVBNM")
 
-databasePass = "Something"
+class DBTools:
 
-mongo = PyMongo(app = None, uri = f"Something{databasePass}Something")
+    def __init__(self, app):
+        self.mongo = PyMongo(app)
 
-def registerSuperUser(username, password):
-    if mongo.db.superAdmin.find({'username' : username}).limit(1).size() != 0:
-        return 'User already exists!'
-    mongo.db.superAdmin.insert({
-        'username' : username,
-        'password' : password,
-        'schools' : []
-    })
-    return 'User added.'
+    def registerSuperUser(self, username, password):
+        if self.mongo.db.superAdmin.find({'username' : username}).limit(1).size() != 0:
+            return 'User already exists!'
+        self.mongo.db.superAdmin.insert({
+            'username' : username,
+            'password' : password,
+            'schools' : []
+        })
+        return 'User added.'
 
-def registerSchool(username, schoolName):
-    schoolID = ''.join(random.choice(CHARSET) for x in range(10))
-    while mongo.db.school.find({'schoolID' : schoolID}).limit(1).size() != 0:
-        schoolID += random.choice(CHARSET)
-    mongo.db.school.insert({ #Registers new school to database
-        'schoolID' : schoolID,
-        'schoolName' : schoolName,
-        'admins' : [],
-        'students' : [],
-        'teachers' : [],
-        'classes' : []
-    })
-    mongo.db.superAdmin.update({ 'username' : username }, { #Add school to user's school list
-        {
-            '$push' : {'schools' : schoolID}
-        }
-    })
-
-if __name__ == '__main__':
-    registerSuperUser('Kevin', '1234')
-    registerSchool('Kevin', 'Stuyvesant High School')
+    def registerSchool(self, username, schoolName):
+        schoolID = ''.join(random.choice(CHARSET) for x in range(10))
+        while self.mongo.db.school.find({'schoolID' : schoolID}).limit(1).size() != 0:
+            schoolID += random.choice(CHARSET)
+        self.mongo.db.school.insert({ #Registers new school to database
+            'schoolID' : schoolID,
+            'schoolName' : schoolName,
+            'admins' : [],
+            'students' : [],
+            'teachers' : [],
+            'classes' : []
+        })
+        self.mongo.db.superAdmin.update({ 'username' : username }, { #Add school to user's school list
+            {
+                '$push' : {'schools' : schoolID}
+            }
+        })
